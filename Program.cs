0c43f1b1561d63d5344e4a57bb9af5f95de2b1f5
iori <iori1001k@gmail.com>
2025-03-12 17:59:38 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("身長を入力してください(m)");
+double height = Convert.ToDouble(Console.ReadLine());
+Console.Write("体重を入力してください(kg)");
+double weight = Convert.ToDouble(Console.ReadLine());
+double bmi = weight / (height * height);
+
+Console.WriteLine("<html><body>");
+Console.WriteLine("<table border>=\"2\">");
+Console.WriteLine($"<tr><td>身長</td><td>{height}</td></tr>");
+Console.WriteLine($"<tr><td>体重</td><td>{weight}</td></tr>");
+Console.WriteLine($"<tr><td>BMI</td><td>{bmi}</td><tr>");
+Console.WriteLine("</table>");
+Console.WriteLine("</body></html>;");
